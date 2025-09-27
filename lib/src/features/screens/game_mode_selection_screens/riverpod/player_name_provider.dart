27b25776f461279fc5b901ac11840ca3af068bf0
chinmoybhakta <djchinmoy9@gmail.com>
2025-09-27 14:28:11 -0700

    feat: difficulty api added

--- lib/src/features/screens/game_mode_selection_screens/riverpod/player_name_provider.dart
+++ lib/src/features/screens/game_mode_selection_screens/riverpod/player_name_provider.dart
@@ -1,8 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final playerNamesProvider = StateProvider<List<String>>((ref) {
-  return ['Player 1', 'Player 2', 'Player 3', 'Player 4'];
-});
+// final playerNamesProvider = StateProvider<List<String>>((ref) {
+//   return ['Player 1', 'Player 2', 'Player 3', 'Player 4'];
+// });
 
 final playerProvider1 = StateProvider<String>((ref)=> "");
 final playerProvider2 = StateProvider<String>((ref)=> "");
